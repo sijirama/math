7349b9c759b5d68d08bccd920d7168c4ab5e7d2a
sijirama <gbemilesanmi@gmail.com>
2025-09-20 15:01:09 +0100

    opencv

--- linear-algebra/image-kernel/main.cpp
+++ linear-algebra/image-kernel/main.cpp
@@ -1,6 +1,19 @@
-#include <iostream> 
+#include <cstdio>
+#include <iostream>
+#include <opencv2/opencv.hpp>
 
 int main() {
-    std::cout << "Hello, Mathematicae!" << std::endl;
+    std::cout << "Project for matric convolution!" << std::endl;
+
+    std::string inputImagePath = "./images/1.jpg";
+    cv::Mat inputImage = cv::imread(inputImagePath);
+    if(!inputImage.data) {
+        std::cerr << "Error: Could not open or find the image." << std::endl;
+        return -1;
+    }
+
+    std::printf("Input image loaded successfully.\n");
+    std::cout << "Image dimensions: " << inputImage.rows << "x" << inputImage.cols << std::endl;
+
     return 0;
 }
